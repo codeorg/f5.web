2e19542cc190ded92610c46cbb0f1d4043f36a05
codeorg <codeasy@163.com>
2016-10-18 10:32:32 +0800

    v1.0.0

---
+++ app/vendor.ts
@@ -0,0 +1,5 @@
+import '@angular/platform-browser';
+//import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+
---
+++ webpack.aot.config.js
@@ -0,0 +1,31 @@
+'use strict';
+var webpack = require('webpack');
+const commonConfig = require('./webpack.common.config.js');
+const webpackMerge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = function(env) {
+  return webpackMerge(commonConfig(), {
+    entry: {
+      'app.aot': './app/bootstrap.aot.ts',
+      'vendor': './app/vendor.ts',
+      //"polyfills":'./app/polyfills.ts'
+    },
+    output: {
+      path: './dist/aot'
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        name: [ 'app.aot','vendor','polyfills']
+      }),
+      new HtmlWebpackPlugin({
+        template: 'index.aot.html',
+        inject: false
+      })
+    ],
+    devServer: {
+      contentBase: 'dist/aot'
+    },
+    bail: true
+  });
+};
